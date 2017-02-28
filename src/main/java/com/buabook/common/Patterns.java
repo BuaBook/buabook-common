76fb6205d31b92a2edcf2996e58e140a13cd5df4
Jaskirat Rajasansir <jas@buabook.com>
2017-02-28 16:29:44 +0000

    Migrate Patterns from internal common library to public repo

---
+++ src/main/java/com/buabook/common/Patterns.java
@@ -0,0 +1,30 @@
+package com.buabook.common;
+
+import java.util.regex.Pattern;
+
+import com.google.common.base.Strings;
+
+/**
+ * <h3>Common Regular Expressions (as {@link Pattern})</h3>
+ * <br/><br/>(c) 2016 Sport Trades Ltd
+ * 
+ * @author Jas Rajasansir
+ * @version 1.0.0
+ * @since 19 Jul 2016
+ */
+public final class Patterns {
+
+	public static final Pattern IS_VALID_EMAIL = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,63}$", Pattern.CASE_INSENSITIVE);
+
+
+	/** 
+	 * @return <code>true</code> if the specified e-mail address is valid, <code>false</code> 
+	 * otherwise (including <code>null</code> or empty string) 
+	 */
+	public static boolean isValidEmail(String emailAddress) {
+		if(Strings.isNullOrEmpty(emailAddress))
+			return false;
+		
+		return IS_VALID_EMAIL.matcher(emailAddress).find();
+	}
+}
---
+++ src/test/java/com/buabook/common/test/PatternsTest.java
@@ -0,0 +1,35 @@
+package com.buabook.common.test;
+
+import org.junit.Test;
+
+import com.buabook.common.Patterns;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class PatternsTest {
+
+	// Patterns.isValidEmail
+	
+	@Test
+	public void testIsValidEmailReturnsFalseForEmptyAndNullEmailAddresses() {
+		assertThat(Patterns.isValidEmail(""), is(equalTo(false)));
+		assertThat(Patterns.isValidEmail(null), is(equalTo(false)));
+	}
+	
+	@Test
+	public void testIsValidEmailReturnsFalseForBadlyFormedEmailAddresses() {
+		assertThat(Patterns.isValidEmail("no-at.com"), is(equalTo(false)));
+		assertThat(Patterns.isValidEmail("no@dot"), is(equalTo(false)));
+		assertThat(Patterns.isValidEmail("two@symbols@com"), is(equalTo(false)));
+		assertThat(Patterns.isValidEmail("white@\r\nspace.com"), is(equalTo(false)));
+		assertThat(Patterns.isValidEmail("quote@\"marks.com"), is(equalTo(false)));
+	}
+	
+	@Test
+	public void testIsValidEmailReturnsTrueForValidEmailAddress() {
+		assertThat(Patterns.isValidEmail("test@email.com"), is(equalTo(true)));
+		assertThat(Patterns.isValidEmail("test@email.co.uk"), is(equalTo(true)));
+	}
+}
+
