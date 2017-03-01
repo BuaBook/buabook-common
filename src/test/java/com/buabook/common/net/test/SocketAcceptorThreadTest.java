5e5183d4de2518b2d42c58b9e9f67f8ff78c4b5e
Jaskirat Rajasansir <jas@buabook.com>
2017-03-01 15:54:14 +0000

    test: Fix SecurityManager to restore after SocketAcceptor tests

--- src/test/java/com/buabook/common/net/test/SocketAcceptorThreadTest.java
+++ src/test/java/com/buabook/common/net/test/SocketAcceptorThreadTest.java
@@ -1,5 +1,8 @@
 package com.buabook.common.net.test;
 
+import java.security.Permission;
+
+import org.junit.AfterClass;
 import org.junit.Test;
 
 import com.buabook.common.connection.Process;
@@ -7,6 +10,15 @@ import com.buabook.common.net.SocketAcceptorThread;
 import com.buabook.common.net.exceptions.FailedToBindToPortException;
 
 public class SocketAcceptorThreadTest {
+	
+	private static SecurityManager existingSecurityManager = System.getSecurityManager();
+	
+	
+	@AfterClass
+	public static void restoreSecurityManager() {
+		System.setSecurityManager(existingSecurityManager);
+	}
+	
 	
 	// Constructor
 	
@@ -29,12 +41,7 @@ public class SocketAcceptorThreadTest {
 
 	@Test(expected=FailedToBindToPortException.class)
 	public void testOpenServerSocketThrowsExceptionIfSecurityException() throws IllegalArgumentException, FailedToBindToPortException {
-		System.setSecurityManager(new SecurityManager() {
-			@Override
-			public void checkListen(int port) {
-				throw new SecurityException();
-			}
-		});
+		System.setSecurityManager(getTestSecurityManager());
 		
 		new SocketAcceptorThread(new Process(null, 23423));
 	}
@@ -44,5 +51,18 @@ public class SocketAcceptorThreadTest {
 		new SocketAcceptorThread(new Process(null, 12345));
 		// Can't bind to the same port twice
 		new SocketAcceptorThread(new Process(null, 12345));
+	}
+	
+	
+	private static SecurityManager getTestSecurityManager() {
+		return new SecurityManager() {
+			@Override
+			public void checkListen(int port) {
+				throw new SecurityException();
+			}
+			
+			@Override
+			public void checkPermission(Permission perm) { }
+		};
 	}
 }
