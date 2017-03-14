befbf9ccd3fb6232b74bd427da6144c04cf153e9
Jaskirat Rajasansir <jas@buabook.com>
2017-03-14 15:44:48 +0000

    exceptions: Remove unused constructors

--- src/main/java/com/buabook/common/net/exceptions/DataSocketFailedToInitialiseException.java
+++ src/main/java/com/buabook/common/net/exceptions/DataSocketFailedToInitialiseException.java
@@ -12,24 +12,9 @@ public class DataSocketFailedToInitialiseException extends Exception {
 	
 	private static final String message = "The data socket failed to initialise, likely due to an issue with the underlying socket.";
 
-	public DataSocketFailedToInitialiseException() {
-		super(message);
-	}
-
-	public DataSocketFailedToInitialiseException(String arg0) {
-		super(message + " " + arg0);
-	}
 
 	public DataSocketFailedToInitialiseException(Throwable arg0) {
 		super(message, arg0);
-	}
-
-	public DataSocketFailedToInitialiseException(String arg0, Throwable arg1) {
-		super(message + " " + arg0, arg1);
-	}
-
-	public DataSocketFailedToInitialiseException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(message + " " + arg0, arg1, arg2, arg3);
 	}
 
 }
--- src/main/java/com/buabook/common/net/exceptions/FailedToBindToPortException.java
+++ src/main/java/com/buabook/common/net/exceptions/FailedToBindToPortException.java
@@ -12,24 +12,8 @@ public class FailedToBindToPortException extends Exception {
 	
 	private static final String message = "ServerSocket failed to bind to the specified host.";
 
-	public FailedToBindToPortException() {
-		super(message);
-	}
-
-	public FailedToBindToPortException(String message) {
-		super(FailedToBindToPortException.message + " " + message);
-	}
 
 	public FailedToBindToPortException(Throwable cause) {
 		super(message, cause);
 	}
-
-	public FailedToBindToPortException(String message, Throwable cause) {
-		super(FailedToBindToPortException.message + " " + message, cause);
-	}
-
-	public FailedToBindToPortException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(FailedToBindToPortException.message + " " + message, cause, enableSuppression, writableStackTrace);
-	}
-
 }
